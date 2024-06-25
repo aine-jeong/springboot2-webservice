d31724dee8dad5b4b6b8f3a2a02eb4f4392675b1
ain <aine0105@naver.com>
2024-06-25 15:20:46 +0900

    4장. 머스테치로 화면 구성하기 / 기본 페이지, CRUD 완료

--- src/main/java/com/ain/study/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/ain/study/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.ain.study.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     /*
@@ -9,4 +12,7 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
     - 단순히 인터페이스를 생성 후 JpaRepository<Entity 클래스, PK 타입>를 상속하면 기본적인 CRUD 메소드가 자동으로 생성된다.
     - *Entity 클래스와 기본 Entity Repository 는 함께 위치해야 한다*
      */
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
 }
--- src/main/java/com/ain/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/ain/study/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.ain.study.springboot.service.posts;
 
 import com.ain.study.springboot.domain.posts.Posts;
 import com.ain.study.springboot.domain.posts.PostsRepository;
+import com.ain.study.springboot.web.dto.PostsListResponseDto;
 import com.ain.study.springboot.web.dto.PostsResponseDto;
 import com.ain.study.springboot.web.dto.PostsSaveRequestDto;
 import com.ain.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,25 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        /*
+        .map(PostsListResponseDto::new)
+        = .map(posts -> new PostsListResponseDto(posts))
+        postsRepository 결과로 넘어온 Posts의 Stream을 map을 통해 PostsListResponseDto 변환 -> List로 반환하는 람다식
+         */
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
