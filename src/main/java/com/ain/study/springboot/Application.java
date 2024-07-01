d6185d5a19139c2171682e5086c452f755b6e0db
ain <aine0105@naver.com>
2024-07-01 15:39:37 +0900

    5장. 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기 / Naver추가, Test코드 수정 완료

--- src/main/java/com/ain/study/springboot/Application.java
+++ src/main/java/com/ain/study/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+//@EnableJpaAuditing // JPA Auditing 활성화 -> 비활성화 (for 테스트)
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
